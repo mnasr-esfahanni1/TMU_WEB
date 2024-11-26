358a7d083ff451a752a57b3d4b7581d17a6cf218
mnasr-esfahanni1 <mnasr-esfahani1@myseneca.ca>
2024-11-26 17:50:29 -0500

    add page

---
+++ assets/js/researchscript.js
@@ -0,0 +1,13 @@
+const locationItems = document.querySelectorAll('.location-item');
+
+locationItems.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.querySelector('.overlay').style.opacity = '1';
+  });
+
+  item.addEventListener('mouseout', () => {
+    item.querySelector('.overlay').style.opacity = '0';
+  });
+});
+
+howto.classList.toggle("show");
